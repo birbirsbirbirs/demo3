582bd8a5d3842eaf3275ed5bffab2696b0c815a9
birbirsbirbirs <birbirsbirbirs@gmail.com>
2024-02-08 23:26:28 -0600

    controller added

---
+++ src/main/java/co/pitam/demo3/controller/HelloWorld.java
@@ -0,0 +1,22 @@
+package co.pitam.demo3.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/hello")
+@RequiredArgsConstructor
+@Slf4j
+public class HelloWorld {
+    @GetMapping
+    public String hello(){
+        String s = "Hello World!! " + UUID.randomUUID();
+        log.info("returning: {}",s);
+        return  s;
+    }
+}
