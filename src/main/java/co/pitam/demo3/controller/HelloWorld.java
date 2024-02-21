15f26eed5d60ca01334bbb61174277096cec35ef
bir <birbirsbirbirs@gmail.com>
2024-02-21 11:26:55 -0600

    rest template set up

--- src/main/java/co/pitam/demo3/controller/HelloWorld.java
+++ src/main/java/co/pitam/demo3/controller/HelloWorld.java
@@ -2,16 +2,18 @@ package co.pitam.demo3.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.UUID;
 
 /**
  * the controller for hello world
  * <p>
- *     the CRUD for hello
+ * the CRUD for hello
  * </p>
  */
 @RestController
@@ -21,23 +23,37 @@ import java.util.UUID;
 public class HelloWorld {
     /**
      * <p>this controller return Hello World!! with random UUID</p>
+     *
      * @return string
      */
+    private final RestTemplate restTemplate;
+
+    @Value("${URL2:null}")
+    private String urlForService2;
+
     @GetMapping
-    public String hello(){
+    public String hello() {
         String s = "Hello World!! " + UUID.randomUUID();
-        log.info("returning: {}",s);
-        return  s;
+        log.info("returning: {}", s);
+        return s;
     }
 
     /**
      * <p>this controller return Beer! Hello World!! with random UUID</p>
+     *
      * @return String
      */
     @GetMapping("/beer")
-    public String helloBeer(){
+    public String helloBeer() {
         String s = "Beer!! Hello World!! " + UUID.randomUUID();
-        log.info("returning: {}",s);
-        return  s;
+        log.info("returning: {}", s);
+        return s;
+    }
+
+    @GetMapping("/service2")
+    public String service2Hello() {
+        String responseFromService2 = restTemplate.getForEntity(urlForService2 + "/api/v1/hello", String.class).getBody();
+        log.info("respond from service 2: {}",responseFromService2);
+        return responseFromService2;
     }
 }
